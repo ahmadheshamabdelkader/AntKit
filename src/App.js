daa6f71efd964eb4038b5d1b67c45557d32468f9
Ahmed Hisham <ahmedhisham@Ahmeds-MacBook-Pro-2.local>
2020-05-08 23:50:06 +0200

    antkit UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Form, Button, Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function handleMenuClick(e) {
+  console.log("click", e);
 }
+
+const menu = (
+  <Menu onClick={handleMenuClick}>
+    <Menu.Item key="1">1st item</Menu.Item>
+    <Menu.Item key="2">2nd item</Menu.Item>
+    <Menu.Item key="3">3rd item</Menu.Item>
+  </Menu>
+);
+
+const App = () => (
+  <div>
+    <Form labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+      <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
+        <Button style={{ marginLeft: 8 }} type="primary">
+          primary
+        </Button>
+        <Button style={{ marginLeft: 8 }}>secondary</Button>
+        <Dropdown overlay={menu}>
+          <Button style={{ marginLeft: 8 }}>
+            Actions <DownOutlined />
+          </Button>
+        </Dropdown>
+      </Form.Item>
+    </Form>
+  </div>
+);
 
 export default App;
